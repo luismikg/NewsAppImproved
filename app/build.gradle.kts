08e7cde4df384f02d3aebb6329e40bc2f786b7a8
Luis Miguel <luismikg@gmail.com>
2025-12-08 20:21:56 -0600

    :recycle: Add Splash screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Splash
+    implementation("androidx.core:core-splashscreen:1.0.0-beta02")
+
 }
