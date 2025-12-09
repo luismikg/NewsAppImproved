3c470e1e5ad21888fcf77979bc6c97c243d7a036
Luis Miguel <luismikg@gmail.com>
2025-12-09 00:17:24 -0600

    :construction: Working on OnBoardingScreen.

--- app/src/main/java/com/luis/newsappcompleted/MainActivity.kt
+++ app/src/main/java/com/luis/newsappcompleted/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.luis.newsappcompleted.presentation.onbording.OnBoardingScreen
 import com.luis.newsappcompleted.ui.theme.NewsAppCompletedTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +26,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppCompletedTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                        OnBoardingScreen(modifier = Modifier.padding(innerPadding))
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/luis/newsappcompleted/presentation/onbording/OnBoardingScreen.kt
@@ -0,0 +1,96 @@
+package com.luis.newsappcompleted.presentation.onbording
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.luis.newsappcompleted.presentation.Dimens
+import com.luis.newsappcompleted.presentation.common.NewsButton
+import com.luis.newsappcompleted.presentation.common.NewsTextButton
+import com.luis.newsappcompleted.presentation.onbording.components.OnBoardingPage
+import com.luis.newsappcompleted.presentation.onbording.components.PagerIndicator
+import kotlinx.coroutines.launch
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(initialPage = 0) {
+            pages.size
+        }
+
+        val buttonState = remember {
+            derivedStateOf {
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    else -> listOf("Back", "Get Started")
+                }
+            }
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnBoardingPage(page = pages[index])
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = Dimens.MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PagerIndicator(
+                modifier = Modifier.width(Dimens.PagerIndicatorWidth),
+                pages.size,
+                selectedPage = pagerState.currentPage
+            )
+
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                val scope = rememberCoroutineScope()
+
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(
+                        text = buttonState.value[0],
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+                            }
+                        }
+                    )
+                }
+                NewsButton(
+                    text = buttonState.value[1],
+                    onClick = {
+                        scope.launch {
+                            if (pagerState.currentPage == 3) {
+                                //TODO: NAvigate to Home Screen
+                            } else {
+                                pagerState.animateScrollToPage(
+                                    page = pagerState.currentPage + 1
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
